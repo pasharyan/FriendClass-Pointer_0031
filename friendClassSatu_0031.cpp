74a8dbd1208e42ae76cb39ec309b194dac7e7a42
pasharyan <pasharyan50@gmail.com>
2026-04-27 11:50:15 +0700

    masukkan class

--- friendClassSatu_0031.cpp
+++ friendClassSatu_0031.cpp
@@ -1,3 +1,25 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class siswa; // Forward declaration
+
+class orang
+{
+private:
+    string nama;
+
+public:
+    void setNama(string pNama);
+    friend class siswa; // Memberikan akses ke class siswa
+};
+
+class siswa
+{
+private:
+    int id;
+
+public:
+    void setId(int pId);
+    void displayAll(orang &a);
+};
